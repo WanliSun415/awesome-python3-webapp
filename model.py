411d21b9fd63981c8e3ff254fa9041e90d77d1a2
swl <672415342@qq.com>
2016-09-22 14:44:00 +0800

    　add Web_model

---
+++ model.py
@@ -0,0 +1,47 @@
+import time
+import uuid
+
+from orm import Model, StringField, BooleanField, FloatField, TextField
+
+
+def next_id():
+    return '%015d%s000' % (int(time.time() * 1000), uuid.uuid4().hex)
+
+
+class User(Model):
+    __table__ = 'users'
+
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    email = StringField(ddl='varchar(50)')
+    passwd = StringField(ddl='varchar(50)')
+    admin = BooleanField()
+    name = StringField(ddl='varchar(50)')
+    image = StringField(ddl='varchar(50)')
+    # 时间和日期用float类型存储在数据库中，不用datetime类型的好处在于不必关心数据库的时区和时区转换，显示时只需要做一个float到str的转换
+    create_at = FloatField(default=time.time)
+
+
+class Blog(Model):
+    __table__ = 'blogs'
+
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    user_id = StringField(ddl='varchar(50)')
+    user_name = StringField(ddl='varchar(50)')
+    user_image = StringField(ddl='varchar(50)')
+    name = StringField(ddl='varchar(50)')
+    summary = StringField(ddl='varchar(50)')
+    content = TextField()
+    create_at = FloatField(defaulr=time.time)
+
+
+class Comment(Model):
+    __table__ = 'comments'
+
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    blog_id = StringField(ddl='varchar(50)')
+    user_id = StringField(ddl='varchar(50)')
+    user_name = StringField(ddl='varchar(50)')
+    user_image = StringField(ddl='varchar(50)')
+    content = TextField
+    create_at = FloatField(defaulr=time.time)
+
