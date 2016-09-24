0954746689a370cdf63a715a5e2fa1fb3384c097
swl <672415342@qq.com>
2016-09-24 18:54:47 +0800

    第一次添加html测试

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ def index(request):
 @asyncio.coroutine
 def init(loop):
     app = web.Application(loop=loop)
+
     app.router.add_route('GET', '/', index)
     srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
     logging.info('server started at http://127.0.0.1:9000...')
