1fb9a2fc5c3c23d33f2b18823fb6963da353a4f2
swl <672415342@qq.com>
2016-09-28 11:45:59 +0800

    删除非day-04文件

--- config/config.py
+++
@@ -1,9 +0,0 @@
-# import config_override
-# import config_default
-config = config_default.configs
-
-try:
-    import config_override
-    config = merge(configs, config_override.configs)
-except ImportError:
-    pass
