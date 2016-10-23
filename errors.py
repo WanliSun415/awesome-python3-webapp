20eead00e9af0dd37b51540ff7e894e059099f25
swl <672415342@qq.com>
2016-10-23 19:36:16 +0800

    修复首页索引问题

--- errors.py
+++ errors.py
@@ -6,7 +6,7 @@ class Page(object):
     '''
     Page object for display pages.
     '''
-    def __init__(self, item_count, page_index=1, page_size=10):
+    def __init__(self, item_count, page_index=1, page_size=10, page_show=3):
         '''
                 Init Pagination by item_count, page_index and page_size.
                 >>> p1 = Page(100, 1)
@@ -30,10 +30,11 @@ class Page(object):
                 90
                 >>> p3.limit
                 10
-                '''
+        '''
         self.item_count = item_count
         self.page_size = page_size
         self.page_count = item_count // page_size + (1 if item_count % page_size > 0 else 0)
+        self.page_show = page_show - 2
         if (item_count == 0) or (page_index > self.page_count):
             self.offset = 0
             self.limit = 0
@@ -50,6 +51,20 @@ class Page(object):
 
     __repr__ = __str__
 
+    def pagelist(self):
+        left = 2
+        right = self.page_count
+
+        if self.page_count > self.page_show:
+            left = self.page_index - (self.page_show // 2)
+            if left < 2:
+                left = 2
+            right = left + self.page_show
+            if right > self.page_count:
+                right = self.page_count
+                left = right - self.page_show
+        # 生成的列表不含首页和末页
+        self.pagelist = list(range(left, right))
 
 
 class APIError(Exception):
--- handlers.py
+++ handlers.py
@@ -75,6 +75,7 @@ async def index(*, page='1'):
     page_index = get_page_index(page)
     num = await Blog.findNumber('count(id)')
     page = Page(num, page_index)
+    page.pagelist()
     if num == 0:
         blogs = []
     else:
